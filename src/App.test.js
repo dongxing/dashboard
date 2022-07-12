1dadf98e4d66601c02802808c80fddec62689d69
Chen Dong Xing <chendongxing87@gmail.com>
2022-07-12 17:00:27 +0800

    Create First Page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders coming soon text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Wah Gwan, Soon Come/i);
   expect(linkElement).toBeInTheDocument();
 });
